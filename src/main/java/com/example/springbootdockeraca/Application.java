56d4a4155127542e05748fd9144924673b9ded8a
Gery Meli <gtatchummeli@iqera.com>
2024-02-13 15:08:56 +0400

    Change port and context path

--- src/main/java/com/example/springbootdockeraca/Application.java
+++ src/main/java/com/example/springbootdockeraca/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
   @RequestMapping("/")
   public String home() {
-    return "Hello Docker World";
+    return "Hello World from Azure Container Apps";
   }
 
   public static void main(String[] args) {
